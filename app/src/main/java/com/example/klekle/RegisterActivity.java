ef90f10d8cabdcfeee666494c23fc73dce71046e
yewon0804 <ehdnjs0804@gmail.com>
2022-11-08 17:36:08 +0900

    Add activity_register.xml & RegisterActivity.java (#1)

--- app/src/main/java/com/example/klekle/RegisterActivity.java
+++ app/src/main/java/com/example/klekle/RegisterActivity.java
@@ -3,12 +3,36 @@ package com.example.klekle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class RegisterActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private EditText join_id, join_pw, join_pw_check, join_nickname;
+    private Button btn_join;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // 툴바 생성
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생성
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ // toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
