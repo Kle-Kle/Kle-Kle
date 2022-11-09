425dde716dc6639e28f6abb4db2d779ac24cba41
yewon0804 <ehdnjs0804@gmail.com>
2022-11-09 19:48:44 +0900

    nav, binding 추가 시도 (#1)

---
+++ app/src/main/java/com/example/klekle/signup/RegisterActivity.kt
@@ -0,0 +1,33 @@
+package com.example.klekle.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.Toolbar
+import com.example.klekle.R
+
+class RegisterActivity : AppCompatActivity() {
+    private var toolbar: Toolbar? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+        // 툴바 생성
+        toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생성
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // toolbar의 back키 눌렀을 때 동작
+                finish()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
