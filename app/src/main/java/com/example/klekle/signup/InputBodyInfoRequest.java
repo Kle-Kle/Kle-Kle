1ad391399342e0ceed3a6d68e2cc9197ae06e5d9
yewon0804 <ehdnjs0804@gmail.com>
2022-11-14 09:22:16 +0900

    Modify RegisterRequest.java, add InputBodyInfoRequest.java (#1)

--- app/src/main/java/com/example/klekle/signup/InputBodyInfoRequest.java
+++ app/src/main/java/com/example/klekle/signup/InputBodyInfoRequest.java
@@ -1,28 +1,27 @@
 package com.example.klekle.signup;
 
-import android.app.Activity;
+import androidx.annotation.Nullable;
 
 import com.android.volley.AuthFailureError;
-import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class RegisterRequest extends StringRequest {
+public class InputBodyInfoRequest extends StringRequest {
     //서버 url 설정(php파일 연동)
-    final static  private String URL = "https://yewon-txuxl.run.goorm.io/klekle/register.php";
-    private Map<String,String>map;
+    final static  private String URL = "https://yewon-txuxl.run.goorm.io/klekle/inputBodyInfo.php";
+    private Map<String,String> map;
 
-    public RegisterRequest(String userid, String userpw, String nickname, Response.Listener<String>listener){
+    public InputBodyInfoRequest(String userid, String height, String weight, String reach, Response.Listener<String>listener){
         super(Method.POST, URL, listener,null); // 위 url에 post방식으로 값을 전송
 
         map = new HashMap<>();
         map.put("userid", userid);
-        map.put("userpw", userpw);
-        map.put("nickname", nickname);
+        map.put("height", height);
+        map.put("weight", weight);
+        map.put("reach", reach);
     }
 
     @Override
