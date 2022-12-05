c9d83b49948481c3e2ddd365ecd3828bebc4dd17
yewon0804 <ehdnjs0804@gmail.com>
2022-12-05 15:53:37 +0900

    Refactoring 시도, 근데 회원가입이 안됨(기능 오류)

--- app/src/main/java/com/example/klekle/signup/InsertUserInfoFragment.kt
+++ app/src/main/java/com/example/klekle/signup/InsertUserInfoFragment.kt
@@ -56,71 +56,84 @@ class InsertUserInfoFragment : Fragment() {
 
             if (isRegularId(userid!!)) {
                 binding.errorId.text = "" // 안내 메세지 숨김
-                if (isRegularPw(userpw!!)) {
-                    binding.errorPw.text = "" // 안내 메세지 숨김
-                    if (userpw.equals(userpwch)) {
-                        if (nickname.equals("")) {
-                            Snackbar.make(view, "닉네임을 설정해 주세요.", Snackbar.LENGTH_SHORT).show();
-                        }
-                        else {
-                            if (email.equals("") || pattern.matcher(email).matches()) {
-                                val responseListener: Response.Listener<String> =
+            }
+            else {
+                binding.errorId.text = "2자 이상의 영어 소문자\n또는 숫자의 조합으로 작성해 주세요."
+            }
+
+            if (isRegularPw(userpw!!)) {
+                binding.errorPw.text = "" // 안내 메세지 숨김
+            }
+            else {
+                binding.errorPw.text = "영문 대/소문자, 숫자, 특수문자(!, ?, *, @, #, \$, %,\n^, &, +, =)를 포함하여 8자 이상 작성해 주세요."
+            }
+
+            if (userpw.equals(userpwch)) {
+                binding.errorPwch.text = ""
+            }
+            else {
+                binding.errorPwch.text = "비밀번호가 일치하지 않습니다."
+                // Snackbar.make(view, "비밀번호가 비밀번호 확인란과 일치하지 않습니다.", Snackbar.LENGTH_LONG).show();
+            }
+
+            if (nickname.equals("")) {
+                binding.errorNick.text = "닉네임을 설정해 주세요."
+                // Snackbar.make(view, "닉네임을 설정해 주세요.", Snackbar.LENGTH_SHORT).show();
+            }
+            else {
+                binding.errorNick.text = "" // 안내 메세지 숨김
+            }
+
+            if (email.equals("") || pattern.matcher(email).matches()) {
+                binding.errorEmail.text = "잊어버린 계정을 되찾을 때 필요합니다."
+            }
+            else {
+                binding.errorEmail.text = "올바른 이메일 형식이 아닙니다."
+                // Snackbar.make(view, "올바른 이메일 형식이 아닙니다.", Snackbar.LENGTH_SHORT).show();
+            }
+
+            if (binding.errorId.text.equals("") && binding.errorPw.text.equals("") && binding.errorPwch.text.equals("") && binding.errorEmail.text.equals("잊어버린 계정을 되찾을 때 필요합니다.")) {
+                val responseListener: Response.Listener<String> =
+                    Response.Listener { response ->
+                        try {
+                            val jsonResponse = JSONObject(response)
+                            val success = jsonResponse.getBoolean("success")
+                            if (success) {
+                                val responseListener2: Response.Listener<String> =
                                     Response.Listener { response ->
                                         try {
-                                            val jsonResponse = JSONObject(response)
-                                            val success = jsonResponse.getBoolean("success")
-                                            if (success) {
-                                                val responseListener2: Response.Listener<String> =
-                                                    Response.Listener { response ->
-                                                        try {
-                                                            val jsonResponse2 = JSONObject(response)
-                                                            val success2 = jsonResponse2.getBoolean("success")
-                                                            if (success2) {
-                                                                // 값 전송
-                                                                val bundle = bundleOf("userid" to userid, "email" to email, "userpw" to userpw, "nickname" to nickname)
-                                                                setFragmentResult("request", bundle)
+                                            val jsonResponse2 = JSONObject(response)
+                                            val success2 = jsonResponse2.getBoolean("success")
+                                            if (success2) {
+                                                // 값 전송
+                                                val bundle = bundleOf("userid" to userid, "email" to email, "userpw" to userpw, "nickname" to nickname)
+                                                setFragmentResult("request", bundle)
 
-                                                                // 넘어가기
-                                                                findNavController().navigate(
-                                                                    R.id.action_InsertUserInfoFragment_to_insertBodyInfoFragment2,
-                                                                    bundle
-                                                                )
-                                                            } else {
-                                                                Snackbar.make(view, "이미 사용 중인 닉네임 입니다.", Snackbar.LENGTH_SHORT).show();
-                                                            }
-                                                        } catch (e: JSONException) {
-                                                            e.printStackTrace()
-                                                        }
-                                                    }
-                                                val nicknameValRequest = NicknameValRequest(nickname, responseListener2)
-                                                val queue2 = Volley.newRequestQueue(context)
-                                                queue2.add(nicknameValRequest)
+                                                // 넘어가기
+                                                findNavController().navigate(
+                                                    R.id.action_InsertUserInfoFragment_to_insertBodyInfoFragment2,
+                                                    bundle
+                                                )
                                             } else {
-                                                Snackbar.make(view, "이미 사용 중인 아이디 입니다.", Snackbar.LENGTH_SHORT).show();
+                                                Snackbar.make(view, "이미 사용 중인 닉네임 입니다.", Snackbar.LENGTH_SHORT).show();
                                             }
                                         } catch (e: JSONException) {
                                             e.printStackTrace()
                                         }
                                     }
-                                val validateRequest = ValidateRequest(userid, responseListener)
-                                val queue = Volley.newRequestQueue(context)
-                                queue.add(validateRequest)
+                                val nicknameValRequest = NicknameValRequest(nickname, responseListener2)
+                                val queue2 = Volley.newRequestQueue(context)
+                                queue2.add(nicknameValRequest)
+                            } else {
+                                Snackbar.make(view, "이미 사용 중인 아이디 입니다.", Snackbar.LENGTH_SHORT).show();
                             }
-                            else {
-                                Snackbar.make(view, "올바른 이메일 형식이 아닙니다.", Snackbar.LENGTH_SHORT).show();
-                            }
+                        } catch (e: JSONException) {
+                            e.printStackTrace()
                         }
                     }
-                    else {
-                        Snackbar.make(view, "비밀번호가 비밀번호 확인란과 일치하지 않습니다.", Snackbar.LENGTH_LONG).show();
-                    }
-                }
-                else {
-                    binding.errorPw.text = "영문 대/소문자, 숫자, 특수문자(!, ?, *, @, #, \$, %,\n^, &, +, =)를 포함하여 8자 이상 작성해 주세요."
-                }
-            }
-            else {
-                binding.errorId.text = "2자 이상의 영어 소문자 또는 숫자의 조합으로 작성해 주세요."
+                val validateRequest = ValidateRequest(userid, responseListener)
+                val queue = Volley.newRequestQueue(context)
+                queue.add(validateRequest)
             }
         }
     }
