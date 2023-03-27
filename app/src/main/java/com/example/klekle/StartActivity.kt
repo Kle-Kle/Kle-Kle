872d9c726b55e054e535a6791464b41b9c5f3db5
yewon0804 <ehdnjs0804@gmail.com>
2023-03-27 22:05:13 +0900

    Modify StartActivity: 화면 전환 방식 변경
    - '시작하기' 버튼을 누르는 게 아니라,
      - 2초 후 자동 전환되는 방식으로 변경

--- app/src/main/java/com/example/klekle/StartActivity.kt
+++ app/src/main/java/com/example/klekle/StartActivity.kt
@@ -3,21 +3,20 @@ package com.example.klekle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Button
 import com.example.klekle.auth.LoginActivity
 
 class StartActivity : AppCompatActivity() {
-    private var btnStart: Button? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
 
-        btnStart = findViewById<View>(R.id.btn_start) as Button
-        btnStart!!.setOnClickListener {
+        Handler().postDelayed(Runnable {
             val intent = Intent(this@StartActivity, LoginActivity::class.java)
+            finishAffinity()
             startActivity(intent)
-        }
+        }, 2000)
     }
 }
