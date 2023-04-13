b0dfc53a54a91911673ecfc0e679164b037cd9f5
yewon0804 <ehdnjs0804@gmail.com>
2023-04-14 01:38:38 +0900

    Update: Main bottom nav - notice badge 추가 (#10)

--- app/src/main/java/com/example/klekle/MainActivity.kt
+++ app/src/main/java/com/example/klekle/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.example.klekle
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.IdRes
 import androidx.appcompat.app.AppCompatActivity
 import com.example.klekle.databinding.ActivityRegisterBinding
 import com.example.klekle.main.CalendarFragment
 import com.example.klekle.main.HomeFragment
 import com.example.klekle.main.MypageFragment
 import com.example.klekle.main.NoticeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
@@ -28,6 +35,9 @@ class MainActivity : AppCompatActivity() {
         val transaction = fragmentManager.beginTransaction()
         transaction.replace(R.id.frame_layout, homeFragment).commitAllowingStateLoss()
 
+        // 알림 badge (임시 고정값)
+        showBadge(this, bottomNavigationView, R.id.notice, "4")
+
         // bottomNavigationView의 아이템이 선택될 때, 호출될 리스너 등록
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             val transaction = fragmentManager.beginTransaction()
@@ -46,6 +56,28 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             true
+        }
+    }
+
+    fun showBadge(
+        context: Context?,
+        bottomNavigationView: BottomNavigationView,
+        @IdRes itemId: Int,
+        value: String?
+    ) {
+        removeBadge(bottomNavigationView, itemId)
+        val itemView = bottomNavigationView.findViewById<BottomNavigationItemView>(itemId)
+        val badge: View = LayoutInflater.from(context)
+            .inflate(R.layout.notification_badge, bottomNavigationView, false)
+        val text = badge.findViewById<TextView>(R.id.notifications_badge)
+        text.text = value
+        itemView.addView(badge)
+    }
+
+    fun removeBadge(bottomNavigationView: BottomNavigationView, @IdRes itemId: Int) {
+        val itemView = bottomNavigationView.findViewById<BottomNavigationItemView>(itemId)
+        if (itemView.childCount == 3) {
+            itemView.removeViewAt(2)
         }
     }
 }
--- app/src/main/java/com/example/klekle/main/NoticeFragment.kt
+++ app/src/main/java/com/example/klekle/main/NoticeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.klekle.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
+import com.example.klekle.CameraActivity
+import com.example.klekle.CommunityActivity
 import com.example.klekle.databinding.FragmentNoticeBinding
 
 class NoticeFragment : Fragment() {
@@ -17,6 +22,10 @@ class NoticeFragment : Fragment() {
     ): View? {
         _binding = FragmentNoticeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
     }
 
     override fun onDestroyView() {
