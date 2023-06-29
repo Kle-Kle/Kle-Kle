3f2fbf72bfeccdc70753a9c0d631a9b04a8e7e4b
yewon0804 <ehdnjs0804@gmail.com>
2023-06-29 23:55:14 +0900

    [FEAT]사용자 신체정보 수정: 입력란에 기존 정보들이 불려오도록 처리 (#7)

---
+++ app/src/main/java/com/example/klekle/main/my/GetBodyRequest.java
@@ -0,0 +1,26 @@
+package com.example.klekle.main.my;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GetBodyRequest extends StringRequest {
+    // 서버 url(API 호출)
+    final static private String URL="https://yewon-txuxl.run.goorm.io/klekle/main/my/getBody.php";
+    private Map<String,String>map;
+
+    public GetBodyRequest(String userid, Response.Listener<String>listener){
+        super(Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("userid", userid);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
--- app/src/main/java/com/example/klekle/main/my/UpdateBodyActivity.java
+++ app/src/main/java/com/example/klekle/main/my/UpdateBodyActivity.java
@@ -3,13 +3,21 @@ package com.example.klekle.main.my;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
 import com.example.klekle.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class UpdateBodyActivity extends AppCompatActivity {
     private Toolbar toolbar;
@@ -21,8 +29,45 @@ public class UpdateBodyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_body);
 
-        btnAmend = (Button) findViewById(R.id.btn_amend);
+        // 툴바 생성
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생성
 
+        inputHeight = findViewById(R.id.input_height);
+        inputWeight = findViewById(R.id.input_weight);
+        inputReach = findViewById(R.id.input_reach);
+
+        // 현재 로그인 중인 사용자의 ID
+        SharedPreferences sharedPreferences = getSharedPreferences("login_info", MODE_PRIVATE);
+        String userid = sharedPreferences.getString("loginedId", null);
+
+        // 기존에 저장돼 있던 신체 정보를 불러옴
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    if (success) {
+                        // Database에 저장되어 있던, 사용자의 신체 정보 가져오기에 성공
+                        inputHeight.setText(jsonObject.getString("height"));
+                        inputWeight.setText(jsonObject.getString("weight"));
+                        inputReach.setText(jsonObject.getString("reach"));
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "신체 정보 불러오기에 실패했습니다.\n잠시 뒤에 다시 시도해 주세요.", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        GetBodyRequest getBodyRequest = new GetBodyRequest(userid, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(UpdateBodyActivity.this);
+        queue.add(getBodyRequest);
+
+        btnAmend = (Button) findViewById(R.id.btn_amend);
         btnAmend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
