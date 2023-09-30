1e9c35e3cc132e3ae175bae7cdf0a40dd71d1ed6
yewon0804 <ehdnjs0804@gmail.com>
2023-09-30 15:09:28 +0900

    FEAT: '앱 정보' 페이지 초안 작성 (#10)

--- app/src/main/java/com/example/klekle/main/my/AppInfoActivity.kt
+++ app/src/main/java/com/example/klekle/main/my/AppInfoActivity.kt
@@ -1,12 +1,49 @@
 package com.example.klekle.main.my
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import com.example.klekle.R
+import com.example.klekle.databinding.ActivityAppInfoBinding
 
-class AppInfoActivity : AppCompatActivity() {
+class AppInfoActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityAppInfoBinding
+
+    lateinit var btnTermsAndConditions : Button
+    lateinit var btnPrivacyPolicy : Button
+    lateinit var btnOpensource : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_info)
+        binding = ActivityAppInfoBinding.inflate(layoutInflater)
+
+        btnTermsAndConditions = findViewById(R.id.btn_terms_and_conditions)
+        btnPrivacyPolicy = findViewById(R.id.btn_privacy_policy)
+        btnOpensource = findViewById(R.id.btn_opensource)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        btnTermsAndConditions.setOnClickListener(this)
+        btnPrivacyPolicy.setOnClickListener(this)
+        btnOpensource.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_terms_and_conditions -> {
+                // todo
+            }
+            R.id.btn_privacy_policy -> {
+                // todo
+            }
+            R.id.btn_opensource -> {
+                // todo
+            }
+        }
     }
 }
