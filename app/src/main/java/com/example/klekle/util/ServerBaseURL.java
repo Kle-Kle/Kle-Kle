6ce5849803212fbfd0936b3afb794ddb797e2b81
yewon0804 <ehdnjs0804@gmail.com>
2023-10-22 01:52:29 +0900

    MODIFY: 서버 base url 수정

--- app/src/main/java/com/example/klekle/util/ServerBaseURL.java
+++ app/src/main/java/com/example/klekle/util/ServerBaseURL.java
@@ -1,7 +1,7 @@
 package com.example.klekle.util;
 
 public class ServerBaseURL {
-    public String baseURL = "http://43.200.192.190:59004/klekle";
+    public String baseURL = "https://ywww-zzauz.run.goorm.site/klekle";
 
     public String getBaseURL() {
         return this.baseURL;
